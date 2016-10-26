46472a7ec76a87aee2fff9acf2c0eec21f436ef2
mvpileg <matthew.pileggi@gmail.com>
2016-10-25 18:26:36 -0700

    Added cocoapods

---
+++ Pods/Target Support Files/Pods-AWSUserPoolsSwift3/Pods-AWSUserPoolsSwift3-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AWSUserPoolsSwift3 : NSObject
+@end
+@implementation PodsDummy_Pods_AWSUserPoolsSwift3
+@end
---
+++ Pods/Target Support Files/Pods-AWSUserPoolsSwift3/Pods-AWSUserPoolsSwift3-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_AWSUserPoolsSwift3VersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_AWSUserPoolsSwift3VersionString[];
+
